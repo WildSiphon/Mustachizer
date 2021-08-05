d5a1b4fa60989d7417e5d132482098dcbed527dd
WildSiphon <matthieularosasimon@gmail.com>
2021-08-05 16:45:29 +0200

    Bot Twitter working

--- twitter_test_script.py
+++ twitter_test_script.py
@@ -1,22 +1,7 @@
-from modules.mustache.mustachizer import Mustachizer
-from urllib.request import urlopen
-from io import BytesIO
-from PIL import ImageShow
+from time import sleep
+from modules.twitter.bot import BotTwitter
 
-url = "https://pbs.twimg.com/media/E7-7oUPXoAMOZWR.jpg"
-must = Mustachizer(debug=True)
-
-url_file = urlopen(url)
-image_buffer = url_file.read()
-output = must.mustachize(BytesIO(image_buffer),"JPEG")
-with open('./img/tmp/0','wb') as out:
-    out.write(output.read())
-
-
-# from time import sleep
-# from modules.twitter.bot import BotTwitter
-
-#twitter = BotTwitter(debug=True)
-# while True:
-# 	twitter.reply_to_last_mentions()
-# 	sleep(10)
+twitter = BotTwitter(debug=True)
+while True:
+	twitter.reply_to_last_mentions()
+	sleep(10)

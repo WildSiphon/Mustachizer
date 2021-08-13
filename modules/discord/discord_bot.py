ebd2e7b4ad019167cec58492a2bfd242edbe0c77
Maël Godey <mael.godey@gmail.com>
2021-08-14 01:21:13 +0200

    Improved bots and formatted code

--- modules/discord/discord_bot.py
+++ modules/discord/discord_bot.py
@@ -1,16 +1,17 @@
 import io
 
 import discord
-from PIL import Image
 
 from modules.mustache.mustachizer import Mustachizer
+from modules.mustache.sentence_provider import SentenceProvider
+from modules.mustache.errors import NoFaceFoundError
 
 
 class DiscordBot(discord.Client):
-
-    def __init__(self):
+    def __init__(self, debug=False):
         super().__init__()
-        self.__mustachizer = Mustachizer()
+        self.__mustachizer = Mustachizer(debug)
+        self.__sentence_provider = SentenceProvider()
 
     async def on_ready(self):
         print("Stachbot ready to mustache !")
@@ -18,13 +19,52 @@ class DiscordBot(discord.Client):
     async def on_message(self, message: discord.Message):
         # Avoid recursion
         if message.author != self.user:
-            if self.user in message.mentions and message.attachments:
+
+            # A user tagged the bot
+            if self.user in message.mentions:
+                print(message.reference.resolved.embeds[0].url)
+
                 mustachized_images = []
-                for attachment in message.attachments:
-                    print(attachment.content_type)
-                    if attachment.content_type.startswith("image/"):
-                        print("Found image !")
-                        buffer = io.BytesIO()
-                        await attachment.save(buffer)
-                        mustachized_images.append(discord.File(self.__mustachizer.mustachize(buffer), filename=attachment.filename))
-                await message.channel.send("Better like that ;)", files = mustachized_images)
+
+                # The message contains a media
+                if message.attachments:
+                    mustachized_images.extend(
+                        await self.mustachize_attachments(message)
+                    )
+
+                # The message is in response of a message containing a media
+                elif message.reference:
+                    original_message = message.reference.resolved
+                    if isinstance(original_message, discord.Message):
+                        mustachized_images.extend(
+                            await self.mustachize_attachments(original_message)
+                        )
+
+                if len(mustachized_images) > 0:
+                    await message.channel.send(
+                        self.__sentence_provider.provide(),
+                        files=mustachized_images,
+                        reference=message,
+                    )
+                else:
+                    await message.channel.send(
+                            "No image or no face found."
+                            )
+
+    async def mustachize_attachments(self, message: discord.Message):
+        mustachized_images = []
+        for attachment in message.attachments:
+            if attachment.content_type.startswith("image/"):
+                buffer = io.BytesIO()
+                await attachment.save(buffer)
+                try:
+                    mustachized_image = self.__mustachizer.mustachize(buffer)
+                    mustachized_images.append(
+                        discord.File(
+                            mustachized_image,
+                            filename=attachment.filename,
+                        )
+                    )
+                except NoFaceFoundError:
+                    pass
+        return mustachized_images
--- modules/twitter/bot.py
+++ modules/twitter/bot.py
@@ -7,10 +7,12 @@ from urllib.request import urlopen
 from io import BytesIO
 from datetime import *
 from modules.mustache.mustachizer import Mustachizer
+from modules.mustache.errors import NoFaceFoundError
 from modules.mustache.sentence_provider import SentenceProvider
 
-#PATH="/home/pi/Bots/Stachebot/"
-PATH="./"
+# PATH="/home/pi/Bots/Stachebot/"
+PATH = "./"
+
 
 class BotTwitter:
     """Start the StacheBot on Twitter.
@@ -19,28 +21,27 @@ class BotTwitter:
     :type debug: bool, optional
     """
 
-    def __init__(self,debug=False):
+    def __init__(self, debug=False):
         """The constructor."""
         self.debug = debug
-        self.lastdate = (datetime.now()-timedelta(hours=2))
-        self.tmp = f'{PATH}img/tmp/'
-        self._empty_tmp()
+        self.lastdate = datetime.now() - timedelta(hours=2)
         self.mustachizer = Mustachizer(debug=False)
         self.sentence_provider = SentenceProvider()
         self._connect()
         self.name = self.api.me().name
         self.screen_name = self.api.me().screen_name
         self.id = self.api.me().id_str
-        if self.debug: print(f"Connected to \'{self.name}\' @{self.screen_name}")
+        if self.debug:
+            print(f"Connected to '{self.name}' @{self.screen_name}")
 
     def _get_credentials(self):
         """Getting the credentials in `credentials.json`"""
-        with open(f'{PATH}modules/twitter/credentials.json','r') as f:
+        with open(f"{PATH}modules/twitter/credentials.json", "r") as f:
             token = json.load(f)
-        self.consumer_key = token['API_KEY']
-        self.consumer_secret = token['API_SECRET_KEY']
-        self.access_token = token['ACCESS_TOKEN']
-        self.access_token_secret = token['ACCESS_TOKEN_SECRET']
+        self.consumer_key = token["API_KEY"]
+        self.consumer_secret = token["API_SECRET_KEY"]
+        self.access_token = token["ACCESS_TOKEN"]
+        self.access_token_secret = token["ACCESS_TOKEN_SECRET"]
 
     def _connect(self):
         """Connect to the Twitter API."""
@@ -48,66 +49,112 @@ class BotTwitter:
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.secure = True
         auth.set_access_token(self.access_token, self.access_token_secret)
-        self.api = tweepy.API(auth,wait_on_rate_limit=True)
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    def _empty_tmp(self):
-        """Empty the dir `img/tmp`."""
-        for filename in os.listdir(self.tmp): os.remove(f"{self.tmp}{filename}")
-
-    def _mustachize_urls(self,urls):
+    def _mustachize_urls(self, urls):
         """Download pictures from urls and mustachize them."""
-        if self.debug: print(f"{len(urls)} pictures :")
-        for count in range(len(urls)):
-            url_file = urlopen(urls[count])
+        if self.debug:
+            print(f"{len(urls)} pictures :")
+        medias = []
+        for url in len(urls):
+            url_file = urlopen(url)
             image_buffer = url_file.read()
-            output = self.mustachizer.mustachize(BytesIO(image_buffer))
-            if output != -1:
-                with open(f"{self.tmp}{count}",'wb') as out:
-                    out.write(output.read())
-                if self.debug:  print(f"\t{count+1}.Done")    
-            elif self.debug:    print(f"\t{count+1}.No Faces")    
+            try:
+                output = self.mustachizer.mustachize(BytesIO(image_buffer))
+                medias.append(output)
+                if self.debug:
+                    print(f"\t{url}.Done")
+            except NoFaceFoundError:
+                if self.debug:
+                    print(f"\t{url}.No Faces")
+        return medias
 
-    def _get_last_mentions(self,max_tweets=1000):
+    def _get_last_mentions(self, max_tweets=1000):
         """Get a list of all mentions that have appeared during the last period of time."""
-        searched_tweets = [status._json for status in tweepy.Cursor(self.api.search, q=f"@{self.screen_name}").items(max_tweets)]
-        last_mentions = [tweet for tweet in searched_tweets if self.screen_name in [t['screen_name'] for t in tweet['entities']['user_mentions']]]
-        self.last_mentions = [tweet for tweet in last_mentions if self.lastdate < datetime.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')]
+        searched_tweets = [
+            status._json
+            for status in tweepy.Cursor(
+                self.api.search, q=f"@{self.screen_name}"
+            ).items(max_tweets)
+        ]
+        last_mentions = [
+            tweet
+            for tweet in searched_tweets
+            if self.screen_name
+            in [t["screen_name"] for t in tweet["entities"]["user_mentions"]]
+        ]
+        self.last_mentions = [
+            tweet
+            for tweet in last_mentions
+            if self.lastdate
+            < datetime.strptime(tweet["created_at"], "%a %b %d %H:%M:%S +0000 %Y")
+        ]
 
-    def _reply_with_twitter_api(self,status='',in_reply_to_status_id=''):
+    def _reply_with_twitter_api(self, medias, status="", in_reply_to_status_id=""):
         """Reply to a tweet with a media"""
-        if len(os.listdir(self.tmp)) != 0:
+        if len(medias) != 0:
             status = self.sentence_provider.provide()
-            media_ids = [self.api.media_upload(f"{self.tmp}{filename}").media_id_string for filename in os.listdir(self.tmp)]
-            self.api.update_status(status=status,media_ids=media_ids,in_reply_to_status_id=in_reply_to_status_id,auto_populate_reply_metadata=True)
+            media_ids = [
+                self.api.media_upload(file=media).media_id_string
+                for media in medias
+            ]
+            self.api.update_status(
+                status=status,
+                media_ids=media_ids,
+                in_reply_to_status_id=in_reply_to_status_id,
+                auto_populate_reply_metadata=True,
+            )
         else:
-            if status == '':
-                status = 'No faces found. Can\'t mustachize :('
-            self.api.update_status(status=status,in_reply_to_status_id=in_reply_to_status_id,auto_populate_reply_metadata=True)
-        if self.debug: print("Replied.")
+            if status == "":
+                status = "No faces found. Can't mustachize :("
+            self.api.update_status(
+                status=status,
+                in_reply_to_status_id=in_reply_to_status_id,
+                auto_populate_reply_metadata=True,
+            )
+        if self.debug:
+            print("Replied.")
 
     def reply_to_last_mentions(self):
         """Responds to all mentions that have appeared during the last period of time."""
         self._get_last_mentions()
-        if self.debug and len(self.last_mentions)!=0:print(f"{datetime.now()-timedelta(hours=2)} GMT +00:00 : {len(self.last_mentions)} new mention")
+        if self.debug and len(self.last_mentions) != 0:
+            print(
+                f"{datetime.now()-timedelta(hours=2)} GMT +00:00 : {len(self.last_mentions)} new mention"
+            )
         for tweet in self.last_mentions:
             self.tweet_with_medias = None
-            if 'media' in tweet['entities']:
-                if self.debug: print("Type \"media\"",end=' ')
+            if "media" in tweet["entities"]:
+                if self.debug:
+                    print('Type "media"', end=" ")
                 self.tweet_with_medias = tweet
-            elif tweet['in_reply_to_status_id_str']:
-                if self.debug: print("Type \"reply\"",end=' ')
-                replying_to = self.api.statuses_lookup([tweet['in_reply_to_status_id_str']])[0]._json
-                if 'media' in replying_to['entities']:
+            elif tweet["in_reply_to_status_id_str"]:
+                if self.debug:
+                    print('Type "reply"', end=" ")
+                replying_to = self.api.statuses_lookup(
+                    [tweet["in_reply_to_status_id_str"]]
+                )[0]._json
+                if "media" in replying_to["entities"]:
                     self.tweet_with_medias = replying_to
                 else:
-                    self._reply_with_twitter_api(status='Could not get any media from the tweet you\'re replying to :(',in_reply_to_status_id=tweet['id_str'])
-                    if self.debug: print("but replying to something with no media")
+                    self._reply_with_twitter_api(
+                        status="Could not get any media from the tweet you're replying to :(",
+                        in_reply_to_status_id=tweet["id_str"],
+                    )
+                    if self.debug:
+                        print("but replying to something with no media")
             else:
                 print("Not a response and no media added to the tweet")
             if self.tweet_with_medias:
-                urls = [media['media_url_https'] for media in self.tweet_with_medias['extended_entities']['media']]
-                self._mustachize_urls(urls)
-                self._reply_with_twitter_api(in_reply_to_status_id=tweet['id_str'])
-                self._empty_tmp()
+                urls = [
+                    media["media_url_https"]
+                    for media in self.tweet_with_medias["extended_entities"]["media"]
+                ]
+                medias = self._mustachize_urls(urls)
+                self._reply_with_twitter_api(
+                    medias, in_reply_to_status_id=tweet["id_str"]
+                )
         if self.last_mentions:
-            self.lastdate = datetime.strptime(self.last_mentions[0]['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+            self.lastdate = datetime.strptime(
+                self.last_mentions[0]["created_at"], "%a %b %d %H:%M:%S +0000 %Y"
+            )

2defec1c7046e4a9324c98b43d3e854ce1a4d918
WildSiphon <wildsiphon@protonmail.com>
2021-12-22 14:27:25 +0100

    Fix discord path

--- main_discord.py
+++ main_discord.py
@@ -1,3 +1,4 @@
+from mustachizer import PATH
 from mustachizer.discord.discord_bot import DiscordBot
 from mustachizer.logging.configuration import ConfigureLogger
 
@@ -7,7 +8,7 @@ ConfigureLogger(log_file="discord_bot", console_level="INFO")
 
 def main():
     token = None
-    with open("./modules/discord/.token") as token_file:
+    with open(PATH / "mustachizer" / "discord" / ".token") as token_file:
         token = token_file.read()
     bot = DiscordBot()
     bot.run(token)
